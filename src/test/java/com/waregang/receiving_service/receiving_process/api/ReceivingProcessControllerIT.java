762283328dd91f195a05c74ebef8335f73c500a4
kamen-kamen <ne.kamenvprostracii@gmail.com>
2026-08-26 15:03:26 +0300

    fix: NPE in test due to missing security context (UserPrincipal)

--- src/test/java/com/waregang/receiving_service/receiving_process/api/ReceivingProcessControllerIT.java
+++ src/test/java/com/waregang/receiving_service/receiving_process/api/ReceivingProcessControllerIT.java
@@ -1,21 +1,30 @@
 package com.waregang.receiving_service.receiving_process.api;
 
 import com.waregang.receiving_service.common.exception_handling.IntegrationMvcTestConfig;
+import com.waregang.receiving_service.receiving_process.api.dto.WorkerStatisticsResponse;
 import com.waregang.receiving_service.receiving_process.application.ReceivingProcessService;
+import com.waregang.receiving_service.security.UserPrincipal;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 
 @ActiveProfiles("test")
 @Import(IntegrationMvcTestConfig.class)
@@ -29,25 +38,51 @@ class ReceivingProcessControllerIT {
     @MockitoBean
     private ReceivingProcessService receivingProcessService;
 
-    @Test
-    @WithMockUser
-    void getWorkerStatistics_ShouldReturnOk_WhenDateIsProvided() {
-        UUID userId = UUID.randomUUID();
+    @BeforeEach
+    void setUpContext() {
+        setSecurityContext();
+    }
 
-        mockMvcTester.get().uri("/api/receiving-service/receiving-sessions/statistics/{userId}", userId)
-                .param("date", LocalDate.now().toString())
-                .accept(MediaType.APPLICATION_JSON)
-                .assertThat()
-                .hasStatusOk();
+    private void setSecurityContext() {
+        UserPrincipal principal = new UserPrincipal(
+                UUID.randomUUID(),
+                "test@test.com",
+                "wh1",
+                List.of(new SimpleGrantedAuthority("WORKER"))
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(principal, null, principal.authorities())
+        );
     }
 
     @Test
-    @WithMockUser
-    void getWorkerStatistics_ShouldReturnOk_WhenDateIsNotProvided() {
-        UUID userId = UUID.randomUUID();
-        mockMvcTester.get().uri("/api/receiving-service/receiving-sessions/statistics/{userId}", userId)
+    void getWorkerStatistics_ShouldReturnStatistics_WhenDateIsProvided() {
+        LocalDate date = LocalDate.now();
+        WorkerStatisticsResponse mockResponse = new WorkerStatisticsResponse(10, 5);
+        
+        Mockito.when(receivingProcessService.getWorkerStatistics(any(), eq(date)))
+                .thenReturn(mockResponse);
+
+        mockMvcTester.get().uri("/api/receiving-service/receiving-sessions/daily-statistics")
+                .param("date", date.toString())
                 .accept(MediaType.APPLICATION_JSON)
                 .assertThat()
-                .hasStatusOk();
+                .hasStatus(HttpStatus.OK)
+                .bodyJson().extractingPath("$.receiptsReceived").asNumber().isEqualTo(10);
+    }
+
+    @Test
+    void getWorkerStatistics_ShouldReturnStatistics_WhenDateIsNotProvided() {
+        WorkerStatisticsResponse mockResponse = new WorkerStatisticsResponse(20, 10);
+        
+        Mockito.when(receivingProcessService.getWorkerStatistics(any(), any(LocalDate.class)))
+                .thenReturn(mockResponse);
+
+        mockMvcTester.get().uri("/api/receiving-service/receiving-sessions/daily-statistics")
+                .accept(MediaType.APPLICATION_JSON)
+                .assertThat()
+                .hasStatus(HttpStatus.OK)
+                .bodyJson().extractingPath("$.receiptsReceived").asNumber().isEqualTo(20);
     }
 }
